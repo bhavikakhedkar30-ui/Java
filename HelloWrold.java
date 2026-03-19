7c3391ea97a3e03204aee579754b812e6bb7c932
unknown <bhavikakhedkar30@gmail.com>
2026-03-19 19:34:58 +0530

    Java Codes

---
+++ HelloWrold.java
@@ -0,0 +1,9 @@
+public class HelloWrold {
+    public static void main(String args[])//THIS IS THE "INT MAIN" OF JAVA//
+    {
+        
+       System.out.println("I'm gonna master java?");
+        
+      
+    }
+}
---
+++ Input.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String args[]){
+        Scanner input = new Scanner(System.in);
+        System.out.println(input.next());// for the first word 
+        System.out.println(input.nextLine());// for the whole line
+    }
+    
+}
