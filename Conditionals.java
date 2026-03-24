4f353be4218063676a3b6f0aac54a9cf77656dfc
unknown <bhavikakhedkar30@gmail.com>
2026-03-24 22:39:10 +0530

    JAVA CODE

---
+++ Conditionals.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Conditionals {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner (System.in);
+        // int n = in.nextInt();
+
+        //FOR LOOP
+         //for(int i= 1 ; i<=n;i++){
+         //    System.out.println(i);
+         //}
+
+
+        // WHILE LOOP 
+        // int x = in.nextInt();
+        // int i=1;
+        // while(i<=x){
+        //     System.out.println(i);
+        //     i++;
+        // }
+
+        //DO WHILE 
+        int n = 1;
+        do{
+            System.out.println(n);
+            n++;
+        }
+        while(n<=5);
+    }
+    
+}
