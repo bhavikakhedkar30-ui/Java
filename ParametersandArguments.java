f18f30873dc6da0c09a6488dc95a756a07e442e2
unknown <bhavikakhedkar30@gmail.com>
2026-03-30 19:53:25 +0530

    Java code

---
+++ ParametersandArguments.java
@@ -0,0 +1,16 @@
+//import java.util.Scanner;
+
+public class ParametersandArguments {
+    public static void main(String[] args) {
+        sum(10,20);
+       
+        
+    }
+
+    static void sum (int a , int b){
+        //Scanner in = new Scanner(System.in);
+         int result = a+b;
+         System.out.println(result);
+
+    }
+}
