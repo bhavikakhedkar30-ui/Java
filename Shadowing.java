f74a2333e661002c2cfbba96d773c8ad47d238e8
unknown <bhavikakhedkar30@gmail.com>
2026-04-01 22:24:38 +0530

    Java code

---
+++ Shadowing.java
@@ -0,0 +1,12 @@
+public class Shadowing {
+    static int x = 20 ;//this will be shadowed at line 6 
+    public static void main(String[] args) {
+        System.out.println(x);
+        int x = 40;
+        System.out.println(x);
+        fun();
+    }
+    static void fun(){
+        System.out.println(x);
+    }
+}
