0ce4bc9953fea6989ccdb4bd50977b31c0c6df52
unknown <bhavikakhedkar30@gmail.com>
2026-04-11 22:01:36 +0530

    Java code

--- SearchAndSort/StringSearch.java
+++ SearchAndSort/StringSearch.java
@@ -1,3 +1,12 @@
 public class StringSearch {
-    
+    public static void main(String[] args) {
+        String name = "Bhavika";
+        char search = 'i';
+
+        for(int i = 0 ; i < name.length() ; i++){
+            if(search == name.charAt(i)){
+                System.out.println(" '"+search+"' "+"found in String --> " + name );
+            }
+        }
+    }
 }
