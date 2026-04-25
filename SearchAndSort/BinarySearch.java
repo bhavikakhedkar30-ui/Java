e53a88bf19b9a8994455aaf9c9eef71e0797977c
unknown <bhavikakhedkar30@gmail.com>
2026-04-25 18:16:45 +0530

    Java Code

---
+++ SearchAndSort/BinarySearch.java
@@ -0,0 +1,15 @@
+public class BinarySearch {
+    public static void main(String[] args) {
+      
+       
+
+    }
+    static int BinarySearch(int [] arr,int target){
+        int str = 0 ;
+        int end = arr.length-1;
+        while(str <= end){
+
+        }
+        return -1;
+    }
+}
