40c3e8dc9d9258ac5c190baef2712eeee79d38f0
unknown <bhavikakhedkar30@gmail.com>
2026-06-06 15:13:23 +0530

    Java Code

--- src/Methods/Syntax.java
+++ src/Methods/Syntax.java
@@ -1,17 +1,25 @@
 package Methods;
-
 public class Syntax {
     public static void bhavika(){
-
         System.out.println("bhavika");
-
-
-
+        BTS();
     }
     static void main() {
-        System.out.println("Hello");
+       // System.out.println("Hello");
         bhavika();
-        System.out.println("Bye");
+        //System.out.println("Bye");
+        //bhavika();
+       // BTS();
+    }
+    public static void BTS(){
+        System.out.println("KIM NAMJOON");
+        System.out.println("KIM SEOKJIN");
+        System.out.println("MIN YOONGI");
+        System.out.println("JUNG HOESOK");
+        System.out.println("PARK JIMIN");
+        System.out.println("KIM TAEHYUNG");
+        System.out.println("JEON JUNGKOOK");
+
         bhavika();
     }
 }
