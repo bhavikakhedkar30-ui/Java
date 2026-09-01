76506ebd2393835c591f725dc38dc545d2e25a24
unknown <bhavikakhedkar30@gmail.com>
2026-09-01 22:44:28 +0530

    Java Code

--- src/Arrays/ArrayListStructure.java
+++ src/Arrays/ArrayListStructure.java
@@ -1,5 +1,7 @@
 package Arrays;
 import java.util.ArrayList;
+import java.util.Collections;
+
 public class ArrayListStructure {
     static void main(String[] args) {
         ArrayList <Integer>  arr = new ArrayList<>();
@@ -12,13 +14,32 @@ public class ArrayListStructure {
         arr.add(60);
         arr.add(70);
         arr.add(80);
+       // arr.remove(6);
 
+        //accessing an element from an arraylist
+        //System.out.println(arr.get(5));
 
-        //accessing an element feom an arraylist
-        System.out.println(arr.get(5));
-
-        //to replace an element in an arraylist with another on a specifc index: -
+        //to replace an element in an arraylist with another on a specific index: -
         arr.set(3,60); // in terms of array :- arr[3] = 60;
+
+        //Method 1 :- print the whole arrayList
+            //  System.out.println(arr);
+
+        //Method 2 :- print the whole arrayList
+        for(int i = 0; i < arr.size(); i++){
+            //System.out.print(arr.get(i)+ " ");
+        }
+
+        //Method 3:- Print the whole arrayList
+        for(int ele :arr){
+          //  System.out.print(ele+" , ");
+        }
+
+        //REVERSE ARRAYLIST//
+
+        Collections.reverse(arr);
+        System.out.println(arr);
+
 
     }
 
