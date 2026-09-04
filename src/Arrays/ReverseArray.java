109de0a7da86887b435652e0fb697ed9f3540011
unknown <bhavikakhedkar30@gmail.com>
2026-09-04 16:20:00 +0530

    Java Code

--- src/Arrays/ReverseArray.java
+++ src/Arrays/ReverseArray.java
@@ -4,17 +4,19 @@ import java.util.Arrays;
 public class ReverseArray {
     static void main(String[] args) {
         int[] arr = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
-        int s = 2;
-        int e = 5;
-        while (s < e){
+        int s = arr[0];
+        int n = arr.length;
+        int e = arr[n - 1];
+        for (int i = 0; i < n; i++) {
+            while (s < e) {
                 int temp = arr[s];
                 arr[s] = arr[e];
                 arr[e] = temp;
                 s++;
                 e--;
             }
-        for (int i = 0; i < arr.length; i++){
-            System.out.print(arr[i]+" ");
+            
+
         }
     }
 }
