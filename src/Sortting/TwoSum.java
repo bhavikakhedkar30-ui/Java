567050e3151db82f72ea309aad2ab0cbf764f872
unknown <bhavikakhedkar30@gmail.com>
2026-09-15 18:09:43 +0530

    Java Code

--- src/Sortting/TwoSum.java
+++ src/Sortting/TwoSum.java
@@ -1,7 +1,24 @@
 package Sortting;
+import java.util.Arrays;
 
 public class TwoSum {
     static void main(String[] args) {
+        int [] arr = {-3,1,8,4,8,7};
+        int target = 9;
+        Arrays.sort(arr);
+        int i = 0;
+        int j = arr.length-1;
+        while (i<j){
+            if(arr[i]+arr[j]==target){
 
+            }
+            else if(arr[i]+arr[j]>target){
+                j--;
+            }
+            else{
+                i++;
+            }
+        }
+        System.out.println("Target number is "+target);
     }
 }
