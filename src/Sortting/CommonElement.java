129208e06088812f819ce3b5c9c5d9190ea10975
unknown <bhavikakhedkar30@gmail.com>
2026-09-15 22:03:28 +0530

    Java Code

---
+++ src/Sortting/CommonElement.java
@@ -0,0 +1,26 @@
+package Sortting;
+
+import java.util.Arrays;
+
+public class CommonElement {
+    static void main(String[] args) {
+        int[] a = {3, 4, 2, 2, 4};
+        int[] b = {3, 2, 2, 7};
+        Arrays.sort(a);
+        Arrays.sort(b);
+        int i = 0;
+        int j = 0;
+        while (i < a.length && j < b.length) {
+
+            if (a[i] == b[j]) {
+                System.out.print(a[i] + " ");
+                i++;
+                j++;
+            } else if (a[i] < b[j]) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+    }
+}
