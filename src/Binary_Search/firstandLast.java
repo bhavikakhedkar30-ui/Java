082719a70b52158d4ea834c4a9137057a957923e
unknown <bhavikakhedkar30@gmail.com>
2026-09-25 00:27:23 +0530

    Java Code

--- src/Binary_Search/firstandLast.java
+++ src/Binary_Search/firstandLast.java
@@ -1,4 +1,26 @@
 package Binary_Search;
 
 public class firstandLast {
+    static void main(String[] args) {
+        int[] arr = {1, 3, 5, 5, 5, 5,5, 67, 123, 125};
+        int x = 5;
+        int firstOcc = -1;
+        int lastOcc = -1;
+        int n = arr.length;
+
+        //last occurrence
+        for (int i = 0; i < n-1; i++) {
+            if (arr[i] == x) {
+                lastOcc = i;
+            }
+        }
+      // System.out.println(firstOcc);
+        for (int i = 0; i < n-1; i++) {
+            if (arr[i] == x) {
+                firstOcc = i;
+                break;
+            }
+        }
+        System.out.println("["+firstOcc+" , "+lastOcc+"]");
+    }
 }
