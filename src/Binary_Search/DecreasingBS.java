d514ab40673e44fb3468a5157224b7fd9881d68b
unknown <bhavikakhedkar30@gmail.com>
2026-09-25 21:34:17 +0530

    Java Code

---
+++ src/Binary_Search/DecreasingBS.java
@@ -0,0 +1,27 @@
+package Binary_Search;
+
+public class DecreasingBS {
+    static void main(String[] args) {
+            int[] arr ={100,90,84,70,65,40};
+            int low = 0;
+            int high = arr.length-1;
+            int tar = 65;
+
+
+           // int ans = 0;
+            while(low<=high){
+                int mid = (low+high)/2;
+                if(arr[mid]==tar){
+                    System.out.println("target found at array index "+mid);
+                  break;
+                }
+                 if(arr[mid]<tar){
+                    high = mid - 1;
+                }
+                 if(arr[mid]>tar){
+                    low = mid + 1;
+                }
+            }
+
+    }
+}
